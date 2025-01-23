dbd97ef1ed6eb3d8226d718a6c7ecc0d99e51e71
liuchuan <liuchuan.huang@flashwire.com>
2025-01-23 17:29:42 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp} from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import pinia from './store';
 import router from './router';
-import './styles/reset.css';
+import './styles/index.less';
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
